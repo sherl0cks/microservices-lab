8c2c1058fec2520c696b57381d7360bb16f3d351
Justin Holmes <jholmes@redhat.com>
2019-01-07 15:26:31 -0700

    adding kafka

--- ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
+++ ticket-msa/order/src/main/java/io/debezium/examples/ticketmsa/order/OrderService.java
@@ -1,6 +1,8 @@
 package io.debezium.examples.ticketmsa.order;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -10,13 +12,25 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import io.debezium.examples.ticketmsa.order.model.Order;
+import org.aerogear.kafka.SimpleKafkaProducer;
+import org.aerogear.kafka.cdi.annotation.KafkaConfig;
+import org.aerogear.kafka.cdi.annotation.Producer;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/orders")
 @ApplicationScoped
+@KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
 public class OrderService {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    @Inject
+    @ConfigProperty(name="order.topic.name", defaultValue="orders")
+    private String topicName;
+
+    @Producer
+    private SimpleKafkaProducer<Integer, JsonObject> kafka;
 
     @POST
     @Transactional
@@ -24,6 +38,7 @@ public class OrderService {
     @Consumes("application/json")
     public Order addOrder(Order order) {
         order = entityManager.merge(order);
+        kafka.send(topicName, order.getId(), order.toJson());
         return order;
     }
 }
